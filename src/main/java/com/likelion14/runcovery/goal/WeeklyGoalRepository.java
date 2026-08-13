879713e3043721f94c3e922116243e0fc2735b72
limjungi <sumy0320@gmail.com>
2026-08-13 14:46:17 +0900

    feat: 주간 목표 생성 API 구현

---
+++ src/main/java/com/likelion14/runcovery/goal/WeeklyGoalRepository.java
@@ -0,0 +1,9 @@
+package com.likelion14.runcovery.goal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface WeeklyGoalRepository extends JpaRepository<WeeklyGoal, Long> {
+    Optional<WeeklyGoal> findTopByFutureGoalOrderByWeekNoDesc(FutureGoal futureGoal);
+}
---
+++ src/main/java/com/likelion14/runcovery/goal/WeeklyScheduleRepository.java
@@ -0,0 +1,6 @@
+package com.likelion14.runcovery.goal;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WeeklyScheduleRepository extends JpaRepository<WeeklySchedule, Long> {
+}
